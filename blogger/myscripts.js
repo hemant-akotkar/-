e8e41ffecf7ac1e7b4e6b7bdedb53103419e617f
hemant-akotkar <144615492+hemant-akotkar@users.noreply.github.com>
2024-05-29 09:16:51 +0530

    Update myscripts.js

--- blogger/myscripts.js
+++ blogger/myscripts.js
@@ -4,7 +4,7 @@
         alert(window.location.href);
     }
             var browsername = window.location.href;
-            if( browsername == "https://rupee-invest.site/" ) {
+            if( browsername == "https://hemant-akotkar.github.io/-/blogger/" ) {
                ;
                } else {
                window.location = "https://data-base-css-krn8393j29jeiekekue7e92.blogspot.com/";
